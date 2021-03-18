c1188fad31850f19fa6734fdd57a9ed635101db1
num24 <num24geek@gmail.com>
2021-03-18 19:19:14 +0900

    Add color to task states

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -11,9 +11,9 @@ class Task extends Model
      * 状態定義
      */
     const STATUS = [
-        1 => [ 'label' => '未着手' ],
-        2 => [ 'label' => '着手中' ],
-        3 => [ 'label' => '完了' ],
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
     ];
 
     /**
@@ -31,5 +31,18 @@ class Task extends Model
         }
 
         return self::STATUS[$status]['label'];
+    }
+
+    public function getStatusClassAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
     }
 }
