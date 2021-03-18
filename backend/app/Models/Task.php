05f1c4a0e574bbd7f069cd5bf66205f96ee4fb7f
num24 <num24geek@gmail.com>
2021-03-18 19:29:50 +0900

    Fix date display to slash separator

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Task extends Model
 {
@@ -44,5 +45,10 @@ class Task extends Model
         }
 
         return self::STATUS[$status]['class'];
+    }
+
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])->format('Y/m/d');
     }
 }
