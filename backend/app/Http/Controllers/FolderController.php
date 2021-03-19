bc693f63f603ea36ed14c3fe5e59e50316bdbc46
num24 <num24geek@gmail.com>
2021-03-19 19:43:46 +0900

    Add validation function to make folder

--- backend/app/Http/Controllers/FolderController.php
+++ backend/app/Http/Controllers/FolderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Folder;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateFolder;
 
 class FolderController extends Controller
 {
@@ -11,7 +12,7 @@ class FolderController extends Controller
     {
         return view('folders/create');
     }
-    public function create(Request $request)
+    public function create(CreateFolder $request)
     {
         //フォルダモデルのインスタンスを作成する
         $folder = new Folder();
@@ -20,7 +21,7 @@ class FolderController extends Controller
         //インスタンスの状態をデータベースに書き込む
         $folder->save();
 
-        return redirect()->route('tasks.index',[
+        return redirect()->route('tasks.index', [
             'id' => $folder->id,
         ]);
     }
--- backend/resources/views/folders/create.blade.php
+++ backend/resources/views/folders/create.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
   <title>ToDo App</title>
   <link rel="stylesheet" href="/css/styles.css">
 </head>
+
 <body>
   <header>
     <nav class="my-navbar">
@@ -20,6 +22,15 @@
           <nav class="panel panel-default">
             <div class="panel-heading">フォルダを追加する</div>
             <div class="panel-body">
+              @if($errors->any())
+              <div class="alert alert-danger">
+                <ul>
+                  @foreach($errors->all() as $message)
+                  <li>{{ $message }}</li>
+                  @endforeach
+                </ul>
+              </div>
+              @endif
               <form action="{{ route('folders.create') }}" method="post">
                 @csrf
                 <div class="form-group">
@@ -37,4 +48,5 @@
     </div>
   </main>
 </body>
+
 </html>
